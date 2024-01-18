f1fd9c3801d45d6bfae86fbff0c62f55df18f7b4
Aban Muhammed C P <abancpchengani@gmail.com>
2024-01-18 08:52:38 +0530

    copyright ceneter->bottum

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ const ToolTip = dynamic(() => import("@/components/home/ToolTip"));
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col h-screen home-bg text-white px-5 md:px-0">
+    <main className="flex flex-col justify-between h-screen home-bg text-white px-5 md:px-0">
       <div className="mt-4 md:mt-8 ml-auto md:px-10 cursor-pointer">
         <ToolTip />
       </div>
@@ -14,15 +14,16 @@ export default function HomePage() {
         <h1 className="font-light text-2xl md:text-3xl">Welcome to</h1>
         <h1 className="font-bold text-4xl md:text-6xl">LunarLoom</h1>
         <Link href="/chat">
-          <button className="bg-blue-500 px-8 py-2 rounded-md my-3 hover:brightness-90">
+          <button className="bg-blue-500 px-8 py-2 rounded-md my-3 hover:brightness-90 ">
             Get Started
           </button>
         </Link>
-        <p className="text-center text-xs md:text-base">
+      </div>
+
+        <p className="text-center mb-1 md:text-base">
           Copyright © 2023 LunarLoom LunarLoom Web Socket Service - WebSocket
           Service for LunarLoom End To End Encrypted Chat App
         </p>
-      </div>
     </main>
   );
 }
