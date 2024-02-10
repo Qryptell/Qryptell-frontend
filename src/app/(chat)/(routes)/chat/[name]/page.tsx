e3a4ac6884d38d362aa7eff87d296dc40a0dab04
Aban Muhammed C P <abancpchengani@gmail.com>
2024-02-10 16:46:05 +0530

    layout updated

--- src/app/(chat)/(routes)/chat/[name]/page.tsx
+++ src/app/(chat)/(routes)/chat/[name]/page.tsx
@@ -11,24 +11,26 @@ type UserChatPageProps = {
 
 export default function UserChatPage({ params: { name } }: UserChatPageProps) {
   return (
-    <main className="bg-black h-full w-full rounded-md flex flex-col">
-      <ChatNavbar name={name} />
-      <div className="flex-1 overflow-y-scroll ml-1 pb-52 pt-4 space-y-6 px-8 scrollbar-hide">
-        <EncryptionMessage />
-        <div className="space-y-2">
-          <ChatMessage msg="hi" />
-          <ChatMessage msg="how are" isUser />
-          <ChatMessage msg="Iam Fine"  />
-          <ChatMessage image={"https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"}  isUser />
-          <ChatMessage msg="Iam Fine" isUser={true} />
-          <ChatMessage msg="Iam Fine" />
-          <ChatMessage msg="Iam Fine" isUser={true} />
-          <ChatMessage msg="Iam Fine" isUser={true} />
-          <ChatMessage msg="Iam Fine" isUser={true} />
-          <ChatMessage isUser={true}  msg="Iam Fine" />
-          <ChatMessage msg="Iam Fine" />
-          <ChatMessage msg="Iam Fine" />
-          <ChatMessage msg="Iam Fine" isUser={true} />
+    <main className="bg-black h-[34rem] w-full  border border-[#525252] rounded-xl ">
+      <div className="h-[32rem] flex flex-col">
+        <ChatNavbar name={name} />
+        <div className="flex-1 overflow-y-scroll ml-1 pb-52 pt-4 space-y-6 px-8 scrollbar-hide">
+          <EncryptionMessage />
+          <div className="space-y-2">
+            <ChatMessage msg="hi" />
+            <ChatMessage msg="how are" isUser />
+            <ChatMessage msg="Iam Fine" />
+            <ChatMessage image={"https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"} isUser />
+            <ChatMessage msg="Iam Fine" isUser={true} />
+            <ChatMessage msg="Iam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn  nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfas" />
+            <ChatMessage msg="Iam Finasdfasdfasdfasdfasdfasdfasfas  asdb asd sajhda dajhd adusn fusadf sdfjasd aasfasde" isUser={true} />
+            <ChatMessage msg="Iam Fine lorem-ipsum Iam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfas " isUser={true} />
+            <ChatMessage msg="Iam Fine" isUser={true} />
+            <ChatMessage isUser={true} msg="Iam Fine" />
+            <ChatMessage msg="Iam Fine" />
+            <ChatMessage msg="Iam Fine" />
+            <ChatMessage msg="Iam Fine" isUser={true} />
+          </div>
         </div>
       </div>
       <ChatInput />
--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -3,20 +3,21 @@ import ChatHeader from "@/components/chat/header/ChatHeader";
 import SideBar from "@/components/chat/sidebar/SideBar";
 import ProtectedRoute from "@/providers/ProtectedRoute";
 import { useUserStore } from "@/store/userStore";
+import { cn } from '@/lib/utils'
 
 export default function ChatLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  let color = 'bg-purple-700'
+  let color = 'bg-black'
   useUserStore((state) => color = state.themeColor)
   return (
     <ProtectedRoute>
-      <main className={`flex ${color} pb-1 overflow-y-hidden h-screen`} >
-        <SideBar themeColor={color} />
-        <section className="h-full w-full flex-1 p-1 flex space-y-1 flex-col text-white">
-          <ChatHeader />
+      <main className={cn(`bg-black overflow-y-hidden h-screen`)} >
+        <ChatHeader />
+        <section className="h-full w-full flex-1 flex pr-5 pb-3 text-white">
+          <SideBar themeColor={color} />
           {children}
         </section>
       </main>
