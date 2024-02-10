29c340f2c84bc5851bc6177b79ba5c609255ad4f
Aban Muhammed C P <abancpchengani@gmail.com>
2024-02-10 16:47:39 +0530

    protected false

--- src/configurations/collection.ts
+++ src/configurations/collection.ts
@@ -2,7 +2,7 @@
 const collection:Collection = {
     SERVER_AUTH_URL: "http://localhost:4000",
     SERVER_USER_URL: "http://localhost:4001",
-    PROTECT: true,
+    PROTECT: false,
 }
 
 export default collection
