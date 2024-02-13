e4d6971eb6a6821047e4eb94cbddc4e3a33fe3a1
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-02-13 11:19:46 +0530

    change in button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function NotFound() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-black">
+    <main className="flex flex-col items-center justify-center min-h-screen">
       <h2 className="text-xl font-bold text-white mt-3">404. Page not found</h2>
 
       <p className="text-lg text-white mt-2">
@@ -10,8 +10,11 @@ function NotFound() {
       </p>
 
       <Link href="/">
-        <button className="px-4 py-2 mt-5 bg-blue-600 hover:bg-blue-700 text-white font-bold rounded-md shadow-md">
-          Home
+        <button
+          className="px-14 py-2.5 mt-5 bg-gradient-to-tr from-[#171616] to-[#4f4f4f]
+         hover:bg-opacity-90 hover:opacity-90 rounded-full"
+        >
+          Back to Home
         </button>
       </Link>
     </main>
