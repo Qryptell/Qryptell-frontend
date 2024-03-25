1fc093bc2ce9eee399fd8b7d0dc5912307e1dfff
Vaishakh GK <vaishakhgk2006@gmail.com>
2024-03-25 11:15:37 +0530

    fix: chat store cannot messags cannot append new message
    chatId, setChatId added to chat store

--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -1,10 +1,12 @@
 import { create } from "zustand";
 
 const useChatStore = create<Chat>((set) => ({
+    chatId: null,
+    setChatId: (chatId) => set({ chatId }),
     messages: [],
-    setMessage: (messages) => set({messages}),
+    addMessage: (message) => set((state) => ({ messages: [...state.messages, message] })),
     webSocket: null,
-    setWebSocket: (webSocket) => set({webSocket}),
+    setWebSocket: (webSocket) => set({ webSocket }),
 }));
 
 export { useChatStore };
