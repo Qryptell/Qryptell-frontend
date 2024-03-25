266241a7891a2b101f9ce74e5118f54fb9a9f087
Vaishakh GK <vaishakhgk2006@gmail.com>
2024-03-25 11:24:30 +0530

    feat: Receive, store, and display messages from server
    Store and display messages sent by the user
    
    Prepare live chat functionality
    
    Remove dummy messages

--- src/app/(chat)/(routes)/chat/[name]/page.tsx
+++ src/app/(chat)/(routes)/chat/[name]/page.tsx
@@ -1,7 +1,9 @@
+"use client"
 import ChatInput from "@/components/chat/ChatInput";
 import ChatMessage from "@/components/chat/ChatMessage";
 import EncryptionMessage from "@/components/chat/EncryptionMessage";
 import ChatNavbar from "@/components/chat/header/ChatNavbar";
+import { useChatStore } from "@/store/chatStore";
 
 type UserChatPageProps = {
   params: {
@@ -11,6 +13,11 @@ type UserChatPageProps = {
 
 export default function UserChatPage({ params: { name: id } }: UserChatPageProps) {
 
+    const messages = useChatStore((state) => state.messages);
+    const setChatId = useChatStore((state) => state.setChatId);
+
+    setChatId(id)
+
   return (
     <main className="bg-black h-[34rem] mb-4 w-full border border-[#525252] rounded-2xl ">
       <div className="h-[30.5rem] flex flex-col">
@@ -18,19 +25,13 @@ export default function UserChatPage({ params: { name: id } }: UserChatPageProps
         <div className="flex-1 overflow-y-scroll pb-52 pt-4 space-y-6 px-4 scrollbar-hide">
           <EncryptionMessage />
           <div className="space-y-2">
-            <ChatMessage msg="hi" />
-            <ChatMessage msg="how are" isUser />
-            <ChatMessage msg="Iam Fine" />
-            <ChatMessage image={"https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"} isUser />
-            <ChatMessage msg="Iam Fine" isUser={true} />
-            <ChatMessage msg="Iam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn  nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfas" />
-            <ChatMessage msg="Iam Finasdfasdfasdfasdfasdfasdfasfas  asdb asd sajhda dajhd adusn fusadf sdfjasd aasfasde" isUser={true} />
-            <ChatMessage msg="Iam Fine lorem-ipsum Iam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfasIam sdbsa nasashd asdbhas dasjdn ajs dasjd ajsdh jasd jasd jasdioasd asi dhsaidh said ioa diad iad aidhsaidh sadsadas skafkslasbasnj m unsmmmefd cl fanaansn mjsmmesd ico ansaban icio nn nmarnnn ;opio yirunal eriyapole nam,irunnu poothopallil njn jnamm l eeriyanam poyathelle nee chirikkum cjunfilar cheelukl p Fineasdasd asdbasd sajhda dajhd adusn fsdsdsf sdf sd fsdf sd fsd fsdusadf sdfjasd asdfasdfas " isUser={true} />
-            <ChatMessage msg="Iam Fine" isUser={true} />
-            <ChatMessage isUser={true} msg="Iam Fine" />
-            <ChatMessage msg="Iam Fine" />
-            <ChatMessage msg="Iam Fine" />
-            <ChatMessage msg="Iam Fine" isUser={true} />
+          {
+              messages.map((message,index) => {
+                  return (
+                      <ChatMessage key={index} msg={message.message} isUser={message.from != id} />
+                  )   
+              })
+          }
           </div>
         </div>
       </div>
